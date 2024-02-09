47fb27dad51a236d8130409e5df5a47c67ac1a42
MetalfaceScout <logan.mathis@utahtech.edu>
2024-02-09 08:09:01 -0700

    Add player migration

---
+++ resources/views/main.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Team Selector</title>
+    </head>
+    <body>
+        <div id="sideBar">
+            <div class="sidebarContent">
+                <a href="/">
+                    <img src="icons/home.png">
+                </a>    
+            </div>
+            <div class="sidebarContent"> 
+                <a href="/edit">
+                    <img src="icons/edit.png">
+                </a>
+            </div>
+        </div>
+        
+        <div id="titleHeader">
+            <h1>Team Selector</h1>
+            <div id="formatSelectDivider">
+                <label for="formatSelect">Choose a team format:</label>
+                <select name="formatSelect" id="formatSelect">
+                    <option value="6playerStandard">6 Players</option>
+                    <option value="5playerStandard">5 Players</option>
+                    <optgroup label="Specials">
+                        <option value="7playerStandard">7 Players</option>
+                        <option value="4playerScouts">4 Players, Scouts</option>
+                        <option value="4playerQueenBee">4 Players, Queen Bee</option>
+                    </optgroup>
+                </select>
+            </div>
+        </div>
+        <div id="mainSection">
+            <div id="infoSection">
+                <button class="createTeamsButton" id="generateRandomTeamsButton">Generate Teams Randomly from Pool</button>
+                <button class="createTeamsButton" id="matchmakeRandom">Matchmake Teams Random</button>
+                <form id="addPlayerForm">
+                    <div>
+                        <label for="addPlayerNameInput">Add Player Name:</label>
+                        <input type="text" id="addPlayerNameInput" name="addPlayerNameInput">
+                    </div>
+                    <div>
+                        <label for="addPlayerRankInput">Add Player Rank:</label>
+                        <input type="text" id="addPlayerRankInput" name="addPlayerRankInput" maxlength="2" size="3">
+                    </div>
+                    <input type="button" value="Add Player" id="addPlayerButton"></input>
+                    <div>
+                        <label for="removePlayerNameInput">Remove Player Name:</label>
+                        <input type="text" id="removePlayerNameInput" name="removePlayerNameInput">
+                    </div>
+                    <input type="button" value="Remove Player" id="removePlayerButton"></input>
+                </form>
+                <div id="playerPool">
+                    <ul id="playerPoolList">
+                        <h2>Fetching average MVP, please wait...</h2>
+                    </ul>
+                </div>
+            </div>
+            <div id="teamSection" >
+                <div id="team1Header" class="teamHeader">
+                    <h2 id="team1name" class="teamname">
+                        Red Team
+                    </h2>
+                    <p id="team1stats"></p>
+                </div>
+                <div id="team2Header" class="teamHeader">
+                    <h2 id="team2name" class="teamname">
+                        Blue Team
+                    </h2>
+                    <p id="team2stats"></p>
+                </div>
+                <div id="team1" class="teamGrid">
+                    
+                </div>
+                
+                <div id="team2" class="teamGrid">
+            
+                </div>
+                <button id="randomizeTeam1" class="randomizeTeamButton">Randomize Team 1</button>
+                <button id="randomizeTeam2" class="randomizeTeamButton">Randomize Team 2</button>
+            </div>
+        </div>
+    </body>
+</html>
